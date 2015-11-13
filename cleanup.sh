ae251c05e8680e9a1d65388cc737326df441cb5a
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2015-11-13 22:27:40 +0000

    Updated translations

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,7 @@
 echo "Cleaning up po files"
 
 # Remove PO files for cross-distro Mint projects
+rm -rf po-export/blueberry*
 rm -rf po-export/cinnamon*
 rm -rf po-export/nemo*
 rm -rf po-export/folder*
