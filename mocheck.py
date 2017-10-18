17fcef4302da1966e9ad0192f1bf6a676b7725a7
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2017-10-18 12:51:16 +0100

    mocheck: Simplify code and threading

--- mocheck.py
+++ mocheck.py
@@ -4,7 +4,7 @@ import polib
 import sys
 import os
 import subprocess
-import thread
+import threading
 import time
 import urllib
 import gi
@@ -29,15 +29,26 @@ GOOD = 0
 BAD_MISCOUNT = 1
 BAD_MISMATCH = 2
 BAD_UNESCAPED_QUOTE = 3
-
 BAD_EXCLUSIONS = 80
-
 BAD_MISCOUNT_MAYBE_DATE = 99
 BAD_MISMATCH_MAYBE_DATE = 100
 
-COL_LANGUAGE = 2
-COL_PROJECT = 8
-COL_ISSUE = 9
+(COL_MO, COL_NUMBER, COL_PROJECT, COL_LANGUAGE, COL_MSGID, COL_MSGSTR, COL_ISSUE) = range(7)
+
+# Used as a decorator to run things in the background
+def async(func):
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=func, args=args, kwargs=kwargs)
+        thread.daemon = True
+        thread.start()
+        return thread
+    return wrapper
+
+# Used as a decorator to run things in the main loop, from another thread
+def idle(func):
+    def wrapper(*args):
+        GObject.idle_add(func, *args)
+    return wrapper
 
 def allowed(char):
     return char in ALLOWED
@@ -76,66 +87,51 @@ class TokenList(list):
 class Mo:
     def __init__(self, inst, locale, path):
         self.mofile = inst
-        self.locale = locale
-        self.path = path
+        self.locale = locale.replace(".po", "").split("-")[-1]
         self.project = path.split("/")[-2]
-        self.bad_entries = []
         self.current_index = 1
 
-class ThreadedTreeView(Gtk.TreeView):
-    def __init__(self, parent, t):
-        Gtk.TreeView.__init__(self)
-        self.type = t
-        self._count = 0
-        self.set_rules_hint(True)
+class Main:
+    def __init__(self, file_type):
+        self.type = file_type
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("mocheck.glade")
+        self.treebox = self.builder.get_object("treebox")
+        self.window = self.builder.get_object("window")
+        self.progressbar = self.builder.get_object("progressbar1")
+        self.window.connect("destroy", Gtk.main_quit)
 
+        self.treeview = Gtk.TreeView()
         column = Gtk.TreeViewColumn("Project", Gtk.CellRendererText(), markup=COL_PROJECT)
-        self.append_column(column)
+        self.treeview.append_column(column)
 
         column = Gtk.TreeViewColumn("Language", Gtk.CellRendererText(), markup=COL_LANGUAGE)
-        self.append_column(column)
+        self.treeview.append_column(column)
 
         cr = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("MsgId", cr, markup=3)
+        column = Gtk.TreeViewColumn("MsgId", cr, markup=COL_MSGID)
         cr.set_property('wrap-mode', Pango.WrapMode.WORD_CHAR)
         cr.set_property('wrap-width', 450)
-        self.append_column(column)
+        self.treeview.append_column(column)
 
         cr = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("MsgStr", cr, markup=4)
+        column = Gtk.TreeViewColumn("MsgStr", cr, markup=COL_MSGSTR)
         column.set_expand(True)
         cr.set_property('wrap-mode', Pango.WrapMode.WORD_CHAR)
         cr.set_property('wrap-width', 450)
-        self.append_column(column)
+        self.treeview.append_column(column)
 
         column = Gtk.TreeViewColumn("Issue", Gtk.CellRendererText(), markup=COL_ISSUE)
-        self.append_column(column)
+        self.treeview.append_column(column)
 
-        self._loading_queue = []
-        self._loading_queue_lock = thread.allocate_lock()
+        self.model = Gtk.TreeStore(object, int, str, str, str, str, str) # (COL_MO, COL_NUMBER, COL_PROJECT, COL_LANGUAGE, COL_MSGID, COL_MSGSTR, COL_ISSUE)
+        self.treeview.set_model(self.model)
 
-        self._loading_lock = thread.allocate_lock()
-        self._loading = False
+        self.treebox.add(self.treeview)
+        self.treeview.connect('button_press_event', self.on_button_press_event)
+        self.window.show_all()
 
-        self._loaded_data = []
-        self._loaded_data_lock = thread.allocate_lock()
-
-    def clear(self):
-        self._count = 0
-        self._loading_queue_lock.acquire()
-        self._loading_queue = []
-        self._loading_queue_lock.release()
-
-        self._loading_lock.acquire()
-        is_loading = self._loading
-        self._loading_lock.release()
-        while is_loading:
-            time.sleep(0.1)
-            self._loading_lock.acquire()
-            is_loading = self._loading
-            self._loading_lock.release()
-        self.model = Gtk.TreeStore(object, object, str, str, str, bool, GdkPixbuf.Pixbuf, int, str, str) # [..], PROJECT, ISSUE
-        self.set_model(self.model)
+        self.load_files()
 
     def get_status_string(self, status):
     	if status == BAD_MISCOUNT:
@@ -151,112 +147,76 @@ class ThreadedTreeView(Gtk.TreeView):
         else:
             return ""
 
-    def _check_loading_progress(self):
-        self._loading_lock.acquire()
-        self._loaded_data_lock.acquire()
-        res = self._loading
-        to_load = []
-        while len(self._loaded_data) > 0:
-            to_load.append(self._loaded_data[0])
-            self._loaded_data = self._loaded_data[1:]
-        self._loading_lock.release()
-        self._loaded_data_lock.release()
+    @idle
+    def add_issue_to_treeview(self, mo, project, msgid, msgstr, issue, current_index):
+        iter = self.model.insert_before(None, None)
+        self.model.set_value(iter, COL_MO, mo)
+        self.model.set_value(iter, COL_PROJECT, project)
+        language = mo.locale
+        self.model.set_value(iter, COL_LANGUAGE, "<span foreground='blue'>%s</span>" % language)
+        self.model.set_value(iter, COL_MSGID, msgid)
+        self.model.set_value(iter, COL_MSGSTR, msgstr)
+        self.model.set_value(iter, COL_ISSUE, self.get_status_string(issue))
+        self.model.set_value(iter, COL_NUMBER, current_index)
 
-        for i in to_load:
-            iter = self.model.insert_before(None, None)
-            self.model.set_value(iter, 0, i[0])
-            self.model.set_value(iter, 1, i[1])
-            if self.type == PO_EXT:
-                language = i[2].replace(".po", "").split("-")[-1]
-                self.model.set_value(iter, COL_LANGUAGE, "<span color='#0000FF'>%s</span>" % language)
-            else:
-                self.model.set_value(iter, 2, i[2])
-            self.model.set_value(iter, 3, i[3])
-            self.model.set_value(iter, 4, i[4])
-            self.model.set_value(iter, 5, False) # dirty flag
-            self.model.set_value(iter, 7, i[5])
-            self.model.set_value(iter, COL_PROJECT, i[0].project)
-            status = i[6]
-            self.model.set_value(iter, COL_ISSUE, self.get_status_string(status))
-            self._count += 1
-            #self.progress.set_text(str(self._count))
-        return res
+    @idle
+    def report_progress(self, count, total):
+        fraction = float(count) / float(total)
+        self.progressbar.set_fraction(fraction)
 
+    @async
     def load_files(self):
-        self.clear()
+        num_files = 0
         for root, subFolders, files in os.walk(os.getcwd(),topdown=False):
             for file in files:
-                if self.type == MO_EXT:
-                    if file.endswith(MO_EXT):
-                        path, junk = os.path.split(root)
-                        path, locale = os.path.split(path)
-                        mo_inst = polib.mofile(os.path.join(root, file))
-                        mo = Mo(mo_inst, locale, os.path.join(root, file))
-                        self.check_file(mo)
+                if self.type == MO_EXT and file.endswith(MO_EXT):
+                    num_files += 1
+                elif file.endswith(PO_EXT):
+                    num_files += 1
+
+        count_files = 0
+        for root, subFolders, files in os.walk(os.getcwd(),topdown=False):
+            for file in files:
+                if self.type == MO_EXT and file.endswith(MO_EXT):
+                    path, junk = os.path.split(root)
+                    path, locale = os.path.split(path)
+                    mo_inst = polib.mofile(os.path.join(root, file))
+                    mo = Mo(mo_inst, locale, os.path.join(root, file))
+                elif file.endswith(PO_EXT):
+                    mo_inst = polib.pofile(os.path.join(root, file))
+                    mo = Mo(mo_inst, file, os.path.join(root, file))
+                    if mo.locale in ["yi"]:
+                        # Don't check PO files for some of the locales (right-to-left languages for instance, or languages where it's hard for us to verify the arguments)
+                        continue
                 else:
-                    if file.endswith(PO_EXT):
-                        locale = file.split("-")[-1].replace(".po", "")
-                        if locale in ["yi"]:
-                            # Don't check PO files for some of the locales (right-to-left languages for instance, or languages where it's hard for us to verify the arguments)
-                            continue
-                        mo_inst = polib.pofile(os.path.join(root, file))
-                        mo = Mo(mo_inst, file, os.path.join(root, file))
-                        self.check_file(mo)
-        #self.progress.set_fraction(1.0)
+                    continue
+                count_files += 1
+                self.check_file(mo)
+                self.report_progress(count_files, num_files)
 
-    def check_file(self, mofile):
-        self._loading_queue_lock.acquire()
-        self._loading_queue.append(mofile)
-        self._loading_queue_lock.release()
-
-        start_loading = False
-        self._loading_lock.acquire()
-        if not self._loading:
-            self._loading = True
-            start_loading = True
-        self._loading_lock.release()
-
-        if start_loading:
-            #self.progress.pulse()
-            GObject.timeout_add(100, self._check_loading_progress)
-            thread.start_new_thread(self._do_load, ())
-
-    def _do_load(self):
-        finished = False
-        while not finished:
-            self._loading_queue_lock.acquire()
-            if len(self._loading_queue) == 0:
-                finished = True
-            else:
-                to_load = self._loading_queue[0]
-                self._loading_queue = self._loading_queue[1:]
-            self._loading_queue_lock.release()
-            if not finished:
-                for entry in to_load.mofile:
-                    if entry.obsolete:
-                        continue # skip obsolete translations (prefixed with #~ in po file)
-                    res = self.check_entry(entry.msgid, entry.msgstr)
-                    if (res > GOOD and res < BAD_MISCOUNT_MAYBE_DATE):
-                        self._loaded_data_lock.acquire()
-                        self._loaded_data.append((to_load, entry, to_load.locale, entry.msgid, entry.msgstr, to_load.current_index, res))
-                        self._loaded_data_lock.release()
-                    if (len(entry.msgstr_plural) > 0):
-                        for plurality in entry.msgstr_plural.keys():
-                            msgstr = entry.msgstr_plural[plurality]
-                            if plurality > 0:
-                                msgid = entry.msgid_plural
-                            else:
-                                msgid = entry.msgid
-                            res = self.check_entry(msgid, msgstr, is_plural=True)
-                            if (res > GOOD and res < BAD_MISCOUNT_MAYBE_DATE):
-                                self._loaded_data_lock.acquire()
-                                self._loaded_data.append((to_load, entry, to_load.locale, msgid, msgstr, to_load.current_index, res))
-                                self._loaded_data_lock.release()
-                    to_load.current_index += 1
-
-        self._loading_lock.acquire()
-        self._loading = False
-        self._loading_lock.release()
+    def check_file(self, mo):
+        for entry in mo.mofile:
+            if entry.obsolete:
+                continue # skip obsolete translations (prefixed with #~ in po file)
+            res = self.check_entry(entry.msgid, entry.msgstr)
+            issue_found = False
+            msgid = entry.msgid
+            msgstr = entry.msgstr
+            if (res != GOOD and res < BAD_MISCOUNT_MAYBE_DATE):
+                issue_found = True
+            if (len(entry.msgstr_plural) > 0):
+                for plurality in entry.msgstr_plural.keys():
+                    msgstr = entry.msgstr_plural[plurality]
+                    if plurality > 0:
+                        msgid = entry.msgid_plural
+                    else:
+                        msgid = entry.msgid
+                    res = self.check_entry(msgid, msgstr, is_plural=True)
+                    if (res != GOOD and res < BAD_MISCOUNT_MAYBE_DATE):
+                        issue_found = True
+            if issue_found:
+                self.add_issue_to_treeview(mo, mo.project, msgid, msgstr, res, mo.current_index)
+            mo.current_index += 1
 
     def check_entry(self, msgid, msgstr, is_plural=False):
         msgid = msgid.replace("%%", " ")
@@ -377,21 +337,25 @@ class ThreadedTreeView(Gtk.TreeView):
                     return BAD_MISMATCH
         return GOOD
 
-class Main:
-    def __init__(self):
-        if len(sys.argv) > 1:
-            if sys.argv[1] == "-po":
-                t = PO_EXT
-                self.start(t)
-            elif sys.argv[1] == "-mo":
-                t = MO_EXT
-                self.start(t)
-            else:
-                self.end()
-        else:
-            self.end()
+    def on_button_press_event(self, widget, event):
+        if event.button == 1 and self.type == PO_EXT:
+            data=widget.get_path_at_pos(int(event.x),int(event.y))
+            if data:
+                path, column, x, y = data
+                if column.get_property('title')=="Language":
+                    iter = self.model.get_iter(path)
+                    project = self.model.get_value(iter, COL_PROJECT)
+                    mo = self.model.get_value(iter, COL_MO)
+                    locale = mo.locale
+                    number = self.model.get_value(iter, COL_NUMBER)
+                    self.go_to_launchpad(project, locale, number)
+                    return False
 
-    def end(self):
+    def go_to_launchpad(self, project, locale, number):
+        os.system("xdg-open 'https://translations.launchpad.net/linuxmint/latest/+pots/%s/%s/%s/+translate'" % (project, locale, number))
+
+
+def usage(self):
         print ""
         print "mocheck: a .po and .mo translation file checker and tweak tool."
         print ""
@@ -404,43 +368,17 @@ class Main:
         print "       mocheck -po : scan recursively from current directory for .po files"
         print "       mocheck -mo : scan recursively from current directory for .mo files"
         print " "
-        quit()
-
-    def start(self, t):
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file("mocheck.glade")
-        self.treebox = self.builder.get_object("treebox")
-        self.window = self.builder.get_object("window")
-
-        self.window.connect("destroy", Gtk.main_quit)
-
-        self.treeview = ThreadedTreeView(self, t)
-        self.treebox.add(self.treeview)
-        self.treeview.connect('button_press_event', self.on_button_press_event)
-        self.window.show_all()
-
-        thread.start_new_thread(self.treeview.load_files, ())
-
-    def on_button_press_event(self, widget, event):
-        if event.button == 1 and self.treeview.type == PO_EXT:
-            data=widget.get_path_at_pos(int(event.x),int(event.y))
-            if data:
-                path, column, x, y = data
-                if column.get_property('title')=="Language":
-                    iter = self.treeview.model.get_iter(path)
-                    project = self.treeview.model.get_value(iter, COL_PROJECT)
-                    pofile = self.treeview.model.get_value(iter, 0)
-                    entry = self.treeview.model.get_value(iter, 1)
-                    number = self.treeview.model.get_value(iter, 7)
-                    locale = pofile.locale
-                    self.go_to_launchpad(project, locale, number)
-                    return False
-
-    def go_to_launchpad(self, project, locale, number):
-        locale = locale.replace(".po", "")
-        locale = locale.split("-")[-1]
-        os.system("xdg-open 'https://translations.launchpad.net/linuxmint/latest/+pots/%s/%s/%s/+translate'" % (project, locale, number))
+        sys.exit(1)
 
 if __name__ == "__main__":
-    Main()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "-po":
+            file_type = PO_EXT
+        elif sys.argv[1] == "-mo":
+            file_type = MO_EXT
+        else:
+                usage()
+    else:
+        usage()
+    Main(file_type)
     Gtk.main()
