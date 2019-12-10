1e8576fdd19c94799b581cd8b2cb5d3ef34becb9
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2019-12-10 17:08:48 +0000

    sort.sh: Installation guide is foreign

--- sort.sh
+++ sort.sh
@@ -27,7 +27,7 @@ for project in ["cinnamon", "cinnamon-bluetooth", "cinnamon-control-center", "ci
         os.system("mv %s FOREIGN/cinnamon-translations/" % project)
 
 # individual projects
-for project in ["blueberry", "lightdm-settings", "mdm", "nemo-emblems", "folder-color-switcher", "pix", "xed", "xplayer", "xreader", "xviewer", "xapp", "slick-greeter", "slideshow-mint", "slideshow-mint-kde", "mintupdate", "mintreport", "xfce4-xapp-status-plugin"]:
+for project in ["blueberry", "lightdm-settings", "mdm", "nemo-emblems", "folder-color-switcher", "pix", "xed", "xplayer", "xreader", "xviewer", "xapp", "slick-greeter", "slideshow-mint", "slideshow-mint-kde", "mintupdate", "mintreport", "xfce4-xapp-status-plugin", "linuxmint-installation-guide"]:
     if os.path.exists(project):
         os.system("mv %s FOREIGN/" % project)
 
