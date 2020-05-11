df1c23acd453162063d52472ad21e2ce6f135787
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2020-05-11 11:52:09 +0100

    sort.sh: Remove obsolete projects mdm and cinnamon-bluetooth

--- sort.sh
+++ sort.sh
@@ -22,28 +22,17 @@ if os.path.exists("xedit") and os.path.exists("xed"):
     os.system("rm -rf xedit")
 
 # cinnamon-translations
-for project in ["cinnamon", "cinnamon-bluetooth", "cinnamon-control-center", "cinnamon-session", "cinnamon-settings-daemon", "cinnamon-screensaver", "nemo", "nemo-extensions"]:
+for project in ["cinnamon", "cinnamon-control-center", "cinnamon-session", "cinnamon-settings-daemon", "cinnamon-screensaver", "nemo", "nemo-extensions"]:
     if os.path.exists(project):
         os.system("mv %s FOREIGN/cinnamon-translations/" % project)
 
 # individual projects
-for project in ["blueberry", "lightdm-settings", "live-installer", "mdm", "nemo-emblems", "folder-color-switcher", "pix", "xed", "xplayer", "xreader", "xviewer", "xapp", "slick-greeter", "slideshow-mint", "slideshow-mint-kde", "mintupdate", "mintreport", "xfce4-xapp-status-plugin", "linuxmint-installation-guide", "warpinator", "mintubiquity", "nvidia-prime-applet"]:
+for project in ["blueberry", "lightdm-settings", "live-installer", "nemo-emblems", "folder-color-switcher", "pix", "xed", "xplayer", "xreader", "xviewer", "xapp", "slick-greeter", "slideshow-mint", "slideshow-mint-kde", "mintupdate", "mintreport", "xfce4-xapp-status-plugin", "linuxmint-installation-guide", "warpinator", "mintubiquity", "nvidia-prime-applet"]:
     if os.path.exists(project):
         os.system("mv %s FOREIGN/" % project)
 
 # projects which require locale.po, as opposed to project-locale.po filenames
-for project in ["folder-color-switcher", "mdm", "pix", "xed", "xplayer", "xreader", "xviewer", "xapp", "slick-greeter", "slideshow-mint", "slideshow-mint-kde", "xfce4-xapp-status-plugin", "mintubiquity", "nvidia-prime-applet"]:
+for project in ["folder-color-switcher", "pix", "xed", "xplayer", "xreader", "xviewer", "xapp", "slick-greeter", "slideshow-mint", "slideshow-mint-kde", "xfce4-xapp-status-plugin", "mintubiquity", "nvidia-prime-applet"]:
     if os.path.exists("FOREIGN/%s" % project):
         os.system("rename 's/%s-//' FOREIGN/%s/*.po" % (project, project))
 
-# mdm requires mo files instead of po files
-if os.path.exists("FOREIGN/mdm"):
-    os.system("mkdir -p FOREIGN/mdm/launchpad-po")
-    for file in os.listdir("FOREIGN/mdm"):
-        if file.endswith(".po"):
-            locale = file.split(".")[0]
-            locale = locale.replace("mdm-", "")
-            os.system("mkdir -p FOREIGN/mdm/files/usr/share/mdm/locale/%s/LC_MESSAGES/" % locale)
-            os.system("msgfmt -o FOREIGN/mdm/files/usr/share/mdm/locale/%s/LC_MESSAGES/mdm.mo FOREIGN/mdm/%s" % (locale, file))
-        if file.endswith(".po") or file.endswith(".pot"):
-            os.system("mv FOREIGN/mdm/%s FOREIGN/mdm/launchpad-po/" % file)
